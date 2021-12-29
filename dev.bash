22e60c4b5d710f5a849f19a47006e4b7209d56ea
Paul Tarvydas <guitarvydas@gmail.com>
2021-12-29 04:45:57 -0500

    dev.bash

--- dev.bash
+++ dev.bash
@@ -1,4 +1,7 @@
 ~/app/make.bash
 cwd=`pwd`
-prep "." "$" appendfacts.ohm appendfacts.glue --stop=1 --support=${cwd}/support.js <appendnames.md
+cp ../query/out .
+prep "." "$" appendfacts.ohm appendfacts.glue --stop=1 --support=${cwd}/support.js <appendnames.md >p2
+chmod a+x p2
+./p2 <out
 
--- support.js
+++ support.js
@@ -3,3 +3,8 @@ let argv;
 exports.setArgv = function (a) {
     argv = a;
 }
+
+exports.trimnl = function (s) {
+    let trimmed = s.replace (/\n/g,'');
+    return trimmed;
+}
