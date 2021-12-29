088d50b2212f36cf50bccf4a5a249310afcd95d6
Paul Tarvydas <guitarvydas@gmail.com>
2021-12-29 04:52:54 -0500

    write out fb.pl, too

--- dev.bash
+++ dev.bash
@@ -1,6 +1,7 @@
 ~/app/make.bash
 cwd=`pwd`
 cp ../query/out .
+cp ../query/fb.pl .
 prep "." "$" appendfacts.ohm appendfacts.glue --stop=1 --support=${cwd}/support.js <appendnames.md >p2
 chmod a+x p2
 ./p2 <out
